584fd11a5697ba85b56073d04532fef52734e05c
Marina Krause Eskelsen <marinakraehh@gmail.com>
2022-07-19 14:35:47 -0300

    All files

---
+++ StrawberryFields.dpr
@@ -0,0 +1,13 @@
+program StrawberryFields;
+
+uses
+  Forms,
+  Morango in 'Morango.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
